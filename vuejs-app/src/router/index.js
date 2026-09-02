895d39975726a3be64edaaff5b879e066829c9e8
chanchakriya <chanriya007@gmail.com>
2026-09-02 14:34:47 +0700

    Router and component(Signin.vue

--- vuejs-app/src/router/index.js
+++ vuejs-app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Signin from '@/auth/Signin.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'auth.signin',
+      component: Signin,
+    }
+  ],
 })
 
 export default router
